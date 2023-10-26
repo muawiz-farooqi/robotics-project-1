5f1a9dd2b9b39761f419ead32e47da07cdc79002
Khorva <102830271+Khorva@users.noreply.github.com>
2023-10-26 18:32:31 -0500

    Added some comments

--- main.py
+++ main.py
@@ -17,45 +17,43 @@ import math
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
-
-# Create your objects here.
 ev3 = EV3Brick()
 right_motor = Motor(Port.A)
 left_motor = Motor(Port.D)
 gyro = GyroSensor(Port.S3)
 
-
 MAX_OBSTACLES = 25
+# The coordinate locations of the obstacles are entered here.
 obstacles = [[0 for x in range(2)] for y in range(MAX_OBSTACLES)]
-obstacles[0] = [0.61, 2.743]
-obstacles[1] = [0.915, 2.743]
-obstacles[2] = [1.219, 2.743]
-obstacles[3] = [1.829, 1.219]
-obstacles[4] = [1.829, 1.524]
-obstacles[5] = [1.829, 1.829]
-obstacles[6] = [1.829, 2.134]
-obstacles[7] = [2.743, 0.305]
-obstacles[8] = [2.743, 0.61]
-obstacles[9] = [2.743, 0.915]
-obstacles[10] = [2.743, 2.743]
-obstacles[11] = [3.048, 2.743]
-obstacles[12] = [3.353, 2.743]
-obstacles[13] = [-1, -1]
-obstacles[14] = [-1, -1]
-obstacles[15] = [-1, -1]
-obstacles[16] = [-1, -1]
-obstacles[17] = [-1, -1]
-obstacles[18] = [-1, -1]
-obstacles[19] = [-1, -1]
-obstacles[20] = [-1, -1]
-obstacles[21] = [-1, -1]
+obstacles[0] = [1.22, 0.305]
+obstacles[1] = [1.22, 0.61]
+obstacles[2] = [1.22, 0.915]
+obstacles[3] = [1.22, 1.22]
+obstacles[4] = [1.22, 1.525]
+obstacles[5] = [2.135, 1.22]
+obstacles[6] = [2.135, 1.525]
+obstacles[7] = [2.135, 1.830]
+obstacles[8] = [2.135, 2.135]
+obstacles[9] = [2.135, 2.440]
+obstacles[10] = [2.135, 2.745]
+obstacles[11] = [1.22, 0]
+obstacles[12] = [3.05, 0.915]
+obstacles[13] = [3.05, 1.22]
+obstacles[14] = [3.05, 1.525]
+obstacles[15] = [3.05, 1.830]
+obstacles[16] = [3.05, 2.135]
+obstacles[17] = [3.355, 0.915]
+obstacles[18] = [3.660, 0.915]
+obstacles[19] = [3.660, 1.22]
+obstacles[20] = [3.965, 0.915]
+obstacles[21] = [3.965, 1.22]
 obstacles[22] = [-1, -1]
 obstacles[23] = [-1, -1]
 obstacles[24] = [-1, -1]
 
 # Goal and start positions
-goal = (3.658, 1.829)
-start = [0.305, 1.219]
+goal = (3.965, 2.135)
+start = [0.61, 0.61]
 workspace = [[-1 for x in range(16)] for y in range(10)]
 
 #Inserting Goal
@@ -78,11 +76,11 @@ for line in reversed(workspace):
 path = findPath(workspace, math.ceil(start[1]/0.305), math.ceil(start[0]/0.305) )
 print(path)
 
-# Write your program here.
 gyro.reset_angle(0)
 left_motor.reset_angle(0)
 right_motor.reset_angle(0)
 
+# Robot runs through the found path to the goal location
 for direction in path:
   # skips the 'Start' direction
   if direction[0] == 'Start':
@@ -126,11 +124,9 @@ for direction in path:
   if gyro.angle() != target_angle:
     correct_direction(gyro, left_motor, right_motor, target_angle)
 
-
   #print("robot moves forward...")
   # Robot moves half a square on the grid. It then checks if its still @ the right
   # orientation. If not, it'll correct itself before moving forward another half square
-
   forward_robot(left_motor, right_motor)
   if gyro.angle() != target_angle:
     correct_direction(gyro, left_motor, right_motor, target_angle)
